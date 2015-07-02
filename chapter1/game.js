472903ce2c0053dff8561d22f2ba9fc3b800a0a7
sdw1211 <sdw1211@gmail.com>
2015-07-03 08:48:23 +0900

    1장소스 완료

--- chapter1/game.js
+++ chapter1/game.js
@@ -1,3 +1,18 @@
-if (jQuery) {
-	$("#question1").show();
-}
+var checkAnswers = function() {
+	var answerString = "";
+	var answers = $(":checked");
+
+	answers.each(function(i) {
+		answerString = answerString + answers[i].value;
+	});
+
+	checkIfCorrect(answerString);
+};
+
+var checkIfCorrect = function(theString) {
+
+	if (parseInt(theString, 16) === 811124566973) {
+		$("body").addClass("correct");
+		$("h1").text("승리했습니다.");
+	}
+};
